b9373afe6003f409777c94e1f32f1dc69470c6d0
Carlos Andrés Montoya Tobón <andresmontoyat@gmail.com>
2021-04-26 10:04:28 -0500

    add github actions

---
+++ .github/actions/packages/lib/packages.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = [
+    'com.meli.mutants.mutants-analyzer',
+    'com.meli.mutants.mutants-api'
+];
---
+++ .github/actions/packages/src/packages.ts
@@ -0,0 +1,4 @@
+export default [
+  'com.meli.mutants.mutants-analyzer',
+  'com.meli.mutants.mutants-api'
+]
